b642785982d3845a9384a95621a798646433f6f3
gokmen.yilmaz <gokmen1977@gmail.com>
2020-07-15 00:27:32 +0300

    dd

--- src/Html2Pdf.jsx
+++ src/Html2Pdf.jsx
@@ -13,14 +13,17 @@ export default class Html2Pdf extends Component {
   exportPDF = () => {
     var doc = new jsPDF();
 
+    let html = `<!DOCTYPE html><html><body><p style="font-family:times">İstanbul</p></body></html>`;
+
+    doc.fromHTML(html, 10, 10);
+
     doc.addFileToVFS("times-normal.ttf", font);
     doc.addFont("times-normal.ttf", "times", "normal");
     doc.setFont("times"); // set font
     doc.setFontSize(50);
 
-    let html = `<!DOCTYPE html><html><body><p style="font-family:times">İstanbul</p></body></html>`;
-
-    doc.fromHTML(html, 10, 10);
+    doc.text("İstanbul", 100, 100);
+    doc.text("Ağaç", 100, 200);
 
     doc.save("a1.pdf");
   };
