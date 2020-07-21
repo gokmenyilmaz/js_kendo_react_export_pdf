062846eff26ba7aba02a9375473f7c859bcc8b81
gokmen.yilmaz <gokmen1977@gmail.com>
2020-07-21 11:18:18 +0300

    fff

--- src/JsKendoPdf.js
+++ src/JsKendoPdf.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "@progress/kendo-theme-default/dist/all.css";
@@ -6,9 +7,23 @@ import products from "./products.json";
 import { DropDownList } from "@progress/kendo-react-dropdowns";
 import { Grid, GridColumn } from "@progress/kendo-react-grid";
 import { PDFExport, savePDF } from "@progress/kendo-react-pdf";
+
+import "froala-editor/js/froala_editor.pkgd.min.js";
+import "froala-editor/css/froala_style.min.css";
+import "froala-editor/css/froala_editor.pkgd.min.css";
+import FroalaEditor from "react-froala-wysiwyg";
+import { floConfig } from "./floConfig.js";
+
+
 import "./pdfstyles.css";
 
 export default class JsKendoPdf extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+
   exportPDFWithMethod = () => {
     savePDF(ReactDOM.findDOMNode(this.container), {
       paperSize: "auto",
@@ -17,9 +32,26 @@ export default class JsKendoPdf extends Component {
     });
   };
 
+  froalaNot=()=>{
+
+    var html=this.myRef.current.editor.html.get();
+
+    // savePDF(<div>{html}</div>, {
+    //   paperSize: "auto",
+    //   margin: 40,
+    //   fileName: `Report for ${new Date().getFullYear()}`,
+    // });
+    console.log(html);
+  }
+
   render() {
     return (
       <div >
+
+        <button onClick={() => this.froalaNot()}>
+          Froala iç
+        </button>
+
         <button onClick={() => this.exportPDFWithMethod()}>
           Kendo Pdf Çıktı
         </button>
@@ -61,6 +93,16 @@ export default class JsKendoPdf extends Component {
           >
             İstanbul Ağaç - bold italic
           </div>
+
+        <div>
+        <FroalaEditor
+          ref={this.myRef}
+          attribution={false}
+          style={{ height: 500, fontSize: "60px" }}
+          config={floConfig}
+        />
+        </div>
+         
 
         </section>
       </div>
